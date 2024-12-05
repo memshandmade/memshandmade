37ce1ddf37e5c00d93cf49b0cc123e04687a23e7
rmoss55s <rmoss55s@gmail.com>
2024-12-05 21:31:18 +0700

    change titleupdate packages

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,10 +1,11 @@
 import { prisma } from '@/lib/prisma'
 import Image from 'next/image'
 import Link from 'next/link'
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react"
 
 export default async function ProductList() {
   const products = await prisma.product.findMany({
-    where: { published: true, soldOut: false },
+    where: { published: true },
   })
 
   // Serialize the products data
@@ -24,6 +25,17 @@ export default async function ProductList() {
           <h2 className="text-xl font-semibold">{product.name}</h2>
           <div className="text-gray-600 mb-2" dangerouslySetInnerHTML={{ __html: product.intro }} />
           <p className="font-bold mb-2">${parseFloat(product.price).toFixed(2)}</p>
+          {product.soldOut? (
+                            <>
+                                
+                                <XCircle className="stroke-destructive"/><p>Sold Out</p>
+                            </>
+                        ) : (
+                            <>
+                                
+                                <p className="text-green-500">Available</p>
+                            </>
+                        )}          
           <Link 
             href={`/products/${product.id}`}
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 inline-block"
