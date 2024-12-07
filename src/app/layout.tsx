6cf3b61ee7e2fa9eead8efebcb802946d56dbc84
rmoss55s <rmoss55s@gmail.com>
2024-12-07 07:32:22 +0700

    ex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "./globals.css";
 import Footer from "@/components/Footer"
 import Image from "next/image";
 import Link from "next/link"
 
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Mem's Handmade",
