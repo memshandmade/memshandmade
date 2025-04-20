38ebca8e3adb75df958f3ce1a511f17c3a7d4364
Mem Moss <memshandmade@gmail.com>
2025-04-20 07:45:38 +0700

    4 cols

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -55,7 +55,7 @@ export default async function ProductList() {
   const products = await getProducts()
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
       {products.map((product) => (
         <div key={product.id} className="border rounded-lg p-4">
           <Image
