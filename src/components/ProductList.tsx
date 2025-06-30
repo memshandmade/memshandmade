f88ba186e11f4f24dd0bd09473f56cb0160c5221
memshandmade <memshandmade@gmail.com>
2025-06-30 09:39:39 +0700

    pics

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -79,7 +79,7 @@ export default function ProductList() {
       </div>
 
       {/* Products Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <div key={product.id} className="border rounded-lg p-4">
             <Image
