7c4cccbba2f0c8ae77bb7671d0ec7a59547efdb1
memshandmade <memshandmade@gmail.com>
2025-07-17 14:46:38 +0700

    foot delay 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
-import Footer from "@/components/Footer"
+
 import Icons from "@/components/HeadIcons"
 import Image from "next/image";
 import Link from "next/link"
